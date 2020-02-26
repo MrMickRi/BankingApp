6be24cea7b881caca9be71af4285d6d240f8809e
Mick Murphy <mickmurphy1993@hotmail.com>
2020-02-26 18:45:09 +0000

    Initial commit

---
+++ src/Main/bankingApp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * @author Mick
+ *
+ */
+public class bankingApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*Welcome to the bank of Apps*");
+		
+		
+		//Create 
+		
+	}
+
+}
